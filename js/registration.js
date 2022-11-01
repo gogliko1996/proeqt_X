9e6c1af6a5666254613f9057fa448e9b84640599
gogliko1996 <chakiduli@gmail.com>
2022-11-01 21:55:08 +0400

    index

--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById("registration");
 
 form.addEventListener("submit", (ivent) => {
@@ -26,13 +27,52 @@ form.addEventListener("submit", (ivent) => {
         GetRegistObjeqt.email = "error";
         document.getElementById("error_email").innerText = "Fill in the field";
     }
-    if(emailValue.match(email)){   
+    if(emailValue.match(email)){ 
+        localStorage.setItem("gmail", emailValue); 
     }else{
-        GetRegistObjeqt.email1 = "error";
+        GetRegistObjeqt.DoNot = "error";
         document.getElementById("error_email").innerText = "Enter a valid email";
     }
 
+    const passwordValue = document.getElementById("password").value;
+    const RpasswordValue = document.getElementById("Rpassword").value;
+    if(passwordValue == "" || passwordValue.length < 4){
+        GetRegistObjeqt.password = "error";
+        // document.getElementById("error_password").innerText = "Enter a valid password";
+    }
+    if(passwordValue !== RpasswordValue || RpasswordValue == ""){
+        GetRegistObjeqt.Rpassword = "error";
+        document.getElementById("error_rpassword").innerText = "Passwords do not match";
+    }else{
+        localStorage.setItem ("password", RpasswordValue);
+    }
+    let clic = false;
+    document.querySelectorAll('[name = "female"]').forEach(element =>{
+        if(element.checked){
+            clic = true;
+        }
+    });
+    if(!clic){
+        document.getElementById("error_gender").innerText = "clic";
+        GetRegistObjeqt.gender = "error";
 
-     console.log(GetRegistObjeqt);
+    }
+    if(Object.keys(GetRegistObjeqt).length == 0){
+        form.setAttribute("action","index.html");
+    }
+})
+
+let eye = document.getElementById("fa-eye-slash");
+let eyePassword = document.getElementById("password");
+eye.addEventListener("click", function (){
+    if(eyePassword.type == "password"){
+        eyePassword.setAttribute("type", "text");
+        eye.classList.add("fa-eye");
+        eye.classList.remove("fa-eye-slash");
+    }else{
+        eyePassword.setAttribute("type", "password");
+        eye.classList.add("fa-eye-slash");
+        eye.classList.remove("fa-eye");
+    }
 })
 
